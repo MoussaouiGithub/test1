b960af986282ba6e11b6af4ae93c795acbc7038f
MoussaouiGithub <116039074+MoussaouiGithub@users.noreply.github.com>
2023-12-29 23:38:30 +0100

    premier changement sur le fichier test1
    j'ai ajouté une deuxième boucle.

--- test.py
+++ test.py
@@ -2,3 +2,9 @@ print("Ce fichier est un fichier de test !")
 
 for i in range(12):
     print('{}^2 = {}'.format(i,i**2))
+
+## Changement effectué sur le site GitHub
+
+tab = []
+for i in range(4):
+    tab.append(i**2)
